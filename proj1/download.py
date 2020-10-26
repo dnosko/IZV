c29aab7456f4c88f7733349c449e424ef0750710
dnosko <dasa.noskova@gmail.com>
2020-10-26 23:21:54 +0100

    init

--- proj1/download.py
+++ proj1/download.py
@@ -1,15 +1,37 @@
+import os
+
 class DataDownloader:
+
+
     def __init__(self,url="https://ehw.fit.vutbr.cz/izv/",folder="data",cache_filename="data_{}.pkl.gz"):
-        pass
+        self.url = url
+        self.folder = folder
+        self.cache_filename = cache_filename
+        self.create_folder(self.folder)
+
+
+    def create_folder(self,folder):
+        try:
+            os.mkdir(folder)
+        except FileExistsError:
+            pass
+
 
     def download_data(self):
         pass
 
+
     def parse_region_data(self,region):
         pass
 
+
     def get_list(self, regions = None):
         pass
+
+
+if __name__ == "__main__":
+    data = DataDownloader()
+    
 
 
 
