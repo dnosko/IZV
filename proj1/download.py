814fb866d364522ddd7a7c411cd22140e38ba861
dnosko <dasa.noskova@gmail.com>
2020-10-26 22:55:02 +0100

    init

---
+++ proj1/download.py
@@ -0,0 +1,18 @@
+class DataDownloader:
+    def __init__(self,url="https://ehw.fit.vutbr.cz/izv/",folder="data",cache_filename="data_{}.pkl.gz"):
+        pass
+
+    def download_data(self):
+        pass
+
+    def parse_region_data(self,region):
+        pass
+
+    def get_list(self, regions = None):
+        pass
+
+
+
+
+
+    
---
+++ proj1/get_stat.py
@@ -0,0 +1,5 @@
+
+import download.py
+
+def plot_stat(data_source, fig_location = None, show_figure = False):
+    data_source = DataDownloader().get)list([3 kraje])
