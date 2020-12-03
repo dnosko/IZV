8a1b6c734e256d36d57ea1662ca9c73f0b706080
dnosko <dasa.noskova@gmail.com>
2020-12-03 22:06:01 +0100

    sum table

--- proj2/analysis.py
+++ proj2/analysis.py
@@ -42,7 +42,20 @@ def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
 # Ukol 2: následky nehod v jednotlivých regionech
 def plot_conseq(df: pd.DataFrame, fig_location: str = None,
                 show_figure: bool = False):
-    pass
+
+    people = df[['region', 'p13a', 'p13b', 'p13c']]
+    melted_table = people.melt(id_vars="region", var_name="types")
+    table = pd.pivot_table(melted_table, columns="types", index="region",values="value", aggfunc=np.sum)
+    print(table)
+
+
+    if fig_location:
+        pass
+        #plt.savefig(fig_location)
+
+    if show_figure:
+        pass
+        #plt.show()
 
 
 # Ukol3: příčina nehody a škoda
@@ -63,6 +76,6 @@ if __name__ == "__main__":
     # skript nebude pri testovani pousten primo, ale budou volany konkreni ¨
     # funkce.
     df = get_dataframe("accidents.pkl.gz", verbose=True)
-    # plot_conseq(df, fig_location="01_nasledky.png", show_figure=True)
+    plot_conseq(df, fig_location="01_nasledky.png", show_figure=True)
     # plot_damage(df, "02_priciny.png", True)
     # plot_surface(df, "03_stav.png", True)
