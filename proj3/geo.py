0ec2987895c5b4816745fb9af1e52e660fbb998c
dnosko <dasa.noskova@gmail.com>
2020-12-21 23:10:08 +0100

    first

--- proj3/geo.py
+++ proj3/geo.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import contextily as ctx
 import sklearn.cluster
 import numpy as np
+
+
 # muzeze pridat vlastni knihovny
 
 
@@ -17,10 +19,10 @@ def _set_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
     columns = list(df)
     del columns[-1]  # delete region
-    #set types
-    type_dic = {'p1': 'int32', 'p13a' :'int32','p13b' :'int32','p13c' :'int32',
-                'p47': 'int32', 'p53':'int32', 'd':'float64','e':'float64',
-                'f':'float64','g':'float64','h':'string','i':'string','k':'string'}
+    # set types
+    type_dic = {'p1': 'int32', 'p13a': 'int32', 'p13b': 'int32', 'p13c': 'int32',
+                'p47': 'int32', 'p53': 'int32', 'd': 'float64', 'e': 'float64',
+                'f': 'float64', 'g': 'float64', 'h': 'string', 'i': 'string', 'k': 'string'}
     df = df.astype(type_dic)
 
     for i in columns:
@@ -29,6 +31,7 @@ def _set_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
+
 def _show_fig(fig_location: str = None, show_figure: bool = False):
     """ Saves figure to given location. If show_figure=True, shows figure. """
     if fig_location:
@@ -36,6 +39,20 @@ def _show_fig(fig_location: str = None, show_figure: bool = False):
 
     if show_figure:
         plt.show()
+
+def _get_min_max(ax):
+    """ najde min a max extremy na osi ax """
+    lims_y = []
+    lims_x = []
+    ymin, ymax = ax.get_ylim()
+    lims_y.append(ymin)
+    lims_y.append(ymax)
+    xmin, xmax = ax.get_xlim()
+    lims_x.append(xmax)
+    lims_x.append(xmin)
+
+    return (lims_x, lims_y)
+
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
@@ -48,6 +65,7 @@ def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
 
     return gdf
 
+
 def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
     """ Vykresleni grafu s dvemi podgrafy podle lokality nehody """
@@ -56,12 +74,13 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     gdf = gdf.loc[gdf['region'] == "JHM"]
 
     # prirad farby a popis k cislam: 1 v obci, 2 mimo obce
-    attrib = {1:("v obci","tab:red"), 2:("mimo obce","tab:blue")}
+    attrib = {1: ("v obci", "tab:red"), 2: ("mimo obce", "tab:blue")}
 
     lims_y = []
     lims_x = []
+    
     # nastav graf
-    fig, axes = plt.subplots(1,2, figsize=(18,8))
+    fig, axes = plt.subplots(1, 2, figsize=(18, 8))
     i = 0
     for ax in axes:
         val = list(attrib.keys())[i]
@@ -70,22 +89,19 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
         gdf[gdf["p5a"] == val].plot(ax=ax, markersize=3, color=color)
         ax.axis("off")
         # najdi min a max hodnoty v kazdom podgrafe
-        ymin, ymax = ax.get_ylim()
-        lims_y.append(ymin)
-        lims_y.append(ymax)
-        xmin, xmax = ax.get_xlim()
-        lims_x.append(xmax)
-        lims_x.append(xmin)
+        x, y = _get_min_max(ax)
+        lims_x = lims_x + x
+        lims_y = lims_y + y
 
         ax.set_title(title)
-        i = i+1
+        i = i + 1
 
     ymax = max(lims_y)
     ymin = min(lims_y)
     xmax = max(lims_x)
     xmin = min(lims_x)
     for ax in axes:
-        ax.set_ylim(ymin,ymax)
+        ax.set_ylim(ymin, ymax)
         ax.set_xlim(xmin, xmax)
         ctx.add_basemap(ax, crs=gdf.crs, source=ctx.providers.Stamen.TonerLite,
                         alpha=0.9)
@@ -93,15 +109,14 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     fig.tight_layout()
     _show_fig(fig_location, show_figure)
 
+
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
     """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
-
 
 
 if __name__ == "__main__":
     # zde muzete delat libovolne modifikace
     gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
     plot_geo(gdf, "geo1.png", True)
-    #plot_cluster(gdf, "geo2.png", True)
-
+    # plot_cluster(gdf, "geo2.png", True)
