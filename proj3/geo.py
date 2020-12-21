5272b8789954668a2be32ea4aafa96376d9b0375
dnosko <dasa.noskova@gmail.com>
2020-12-21 23:06:21 +0100

    first done

--- proj3/geo.py
+++ proj3/geo.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import geopandas
 import matplotlib.pyplot as plt
-import contextily
+import contextily as ctx
 import sklearn.cluster
 import numpy as np
 # muzeze pridat vlastni knihovny
@@ -29,6 +29,14 @@ def _set_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
+def _show_fig(fig_location: str = None, show_figure: bool = False):
+    """ Saves figure to given location. If show_figure=True, shows figure. """
+    if fig_location:
+        plt.savefig(fig_location)
+
+    if show_figure:
+        plt.show()
+
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
     df = _set_dataframe(df)
@@ -44,6 +52,46 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
     """ Vykresleni grafu s dvemi podgrafy podle lokality nehody """
 
+    # vyfiltruj iba jeden region - JHM
+    gdf = gdf.loc[gdf['region'] == "JHM"]
+
+    # prirad farby a popis k cislam: 1 v obci, 2 mimo obce
+    attrib = {1:("v obci","tab:red"), 2:("mimo obce","tab:blue")}
+
+    lims_y = []
+    lims_x = []
+    # nastav graf
+    fig, axes = plt.subplots(1,2, figsize=(18,8))
+    i = 0
+    for ax in axes:
+        val = list(attrib.keys())[i]
+        color = attrib[val][1]
+        title = "Nehody v JHM kraji: " + attrib[val][0]
+        gdf[gdf["p5a"] == val].plot(ax=ax, markersize=3, color=color)
+        ax.axis("off")
+        # najdi min a max hodnoty v kazdom podgrafe
+        ymin, ymax = ax.get_ylim()
+        lims_y.append(ymin)
+        lims_y.append(ymax)
+        xmin, xmax = ax.get_xlim()
+        lims_x.append(xmax)
+        lims_x.append(xmin)
+
+        ax.set_title(title)
+        i = i+1
+
+    ymax = max(lims_y)
+    ymin = min(lims_y)
+    xmax = max(lims_x)
+    xmin = min(lims_x)
+    for ax in axes:
+        ax.set_ylim(ymin,ymax)
+        ax.set_xlim(xmin, xmax)
+        ctx.add_basemap(ax, crs=gdf.crs, source=ctx.providers.Stamen.TonerLite,
+                        alpha=0.9)
+
+    fig.tight_layout()
+    _show_fig(fig_location, show_figure)
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
@@ -54,6 +102,6 @@ def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
 if __name__ == "__main__":
     # zde muzete delat libovolne modifikace
     gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
-    #plot_geo(gdf, "geo1.png", True)
+    plot_geo(gdf, "geo1.png", True)
     #plot_cluster(gdf, "geo2.png", True)
 
