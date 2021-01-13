4bdb106118bcc067a57cafc2a31bcdde2149b8bf
dnosko <dasa.noskova@gmail.com>
2021-01-13 22:57:32 +0100

    doc.py

--- proj3/doc.py
+++ proj3/doc.py
@@ -28,26 +28,38 @@ def _set_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-def plot_graph(df: pd.DataFrame) -> pd.DataFrame:
-    #  sort data to bins
-    accident_type = ['nezaviňená řidičem', 'nepřiměřená rychlost jízdy', 'nesprávné předjíždění',
-                     'nedání přednosti v jízdě', 'nesprávný způsob jízdy', 'technická závada vozidla']
-    bins = [100, 201, 301, 401, 501, 601, 616]
-    df['p12'] = pd.cut(df['p12'], bins, labels=accident_type, right=False)
-
-    people = df[['p12', 'p13a', 'p13b', 'p13c']]
-    melted_table = people.melt(id_vars="p12", var_name="types")
-    table = pd.pivot_table(melted_table, columns="types", index="p12", values="value", aggfunc=["sum", "count"])
+def mold_table(cols, names, df: pd.DataFrame):
+    """ Sets table to desired structure.
+     """
+    main_col = cols[0]
+    people = df[cols]
+    melted_table = people.melt(id_vars=main_col, var_name="types")
+    table = pd.pivot_table(melted_table, columns="types", index=main_col, values="value", aggfunc=["sum", "count"])
     #  drop repeating columns
     table = table.drop(table.columns[[4, 5]], axis=1)
     #  rename last column to count
-    table.columns = ['Úmrtia', 'Ťažko zranení', 'Ľahko zranení', 'count']
+    table.columns = names
     #  sort table
     table = table.reindex(table.sort_values(by='count', ascending=False).index)
 
     table = table.reset_index()
 
-    # plo
+    return table
+
+def sort_to_bins(df: pd.DataFrame, labels, bins, col):
+    """ Sort column to bins and labels the bins """
+    df[col] = pd.cut(df[col], bins, labels=labels, right=False)
+    return df
+
+def plot_graph(df: pd.DataFrame) -> pd.DataFrame:
+    #  sort data to bins
+    accident_type = ['nezaviňená řidičem', 'nepřiměřená rychlost jízdy', 'nesprávné předjíždění',
+                     'nedání přednosti v jízdě', 'nesprávný způsob jízdy', 'technická závada vozidla']
+
+    df = sort_to_bins(df, accident_type, [100, 201, 301, 401, 501, 601, 616], 'p12')
+
+    table = mold_table(['p12', 'p13a', 'p13b', 'p13c'],['Úmrtia', 'Ťažko zranení', 'Ľahko zranení', 'count'],df )
+
     plot(table)
 
     return table
@@ -98,6 +110,44 @@ def plot(df: pd.DataFrame):
     fig.tight_layout()
     plt.savefig('fig.png')
 
+
+def substances_deaths(df: pd.DataFrame):
+    """ Most deaths by drivers under influence in average: """
+
+    alcohol = ['alkohol do 0.24', 'triezvy', 'alkohol do 0.5','drogy','drogy a alkohol',
+                     'alkohol do 1.0', 'alkohol do 1.5', 'alkohol viac ako 1.5']
+    bins = [1, 2, 3, 4, 5, 7,8,9,10]
+    df['p11'] = pd.cut(df['p11'], bins, labels=alcohol, right=False)
+
+    accident_type = ['nezaviňená řidičem', 'nepřiměřená rychlost jízdy', 'nesprávné předjíždění',
+                     'nedání přednosti v jízdě', 'nesprávný způsob jízdy', 'technická závada vozidla']
+
+    df = sort_to_bins(df, accident_type, [100, 201, 301, 401, 501, 601, 616], 'p12')
+    # aggregate counts
+    table = df.p11.groupby([df.p12, df.p11]).count().unstack().fillna(0).astype(int)
+    # set total
+    table.columns = table.columns.add_categories(['Celkom'])
+    table['Celkom'] = table.sum(axis=1)
+    # transpose - switch cols and rows
+    table = table.T
+    # print table to output
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print('LATEX:')
+        print(table.to_latex(index=True))
+
+    print_substances('p13a',['Úmrtia','úmrtí'])
+    print_substances('p13b', ['Tažké zranenia', 'zranení'])
+
+def print_substances(col, text):
+    table = df.groupby('p11')[col].agg(['sum', 'count', 'mean']).reset_index()
+    table = table.sort_values(by='mean', ascending=False)
+    print(table)
+    print(text[0],"sú najčastejšie spôsobené vodičom pod vplyvom:")
+    for i in range(3):
+        print(table.iat[i, 0], ' priemerne:', "{:.3f}".format(table.iat[i, 3]), text[1])
+    print(table.iat[-1, 0], ' priemerne:', "{:.3f}".format(table.iat[-1, 3]), text[1])
+
+
 def print_values(graf: pd.DataFrame):
     graf = graf.rename(columns={'p12':'Príčina nehody'})
     graf = graf.set_index('Príčina nehody')
@@ -106,6 +156,7 @@ def print_values(graf: pd.DataFrame):
 if __name__ == "__main__":
     df = pd.read_pickle("accidents.pkl.gz", compression='gzip')
     df = _set_dataframe(df)
-    graf = plot_graph(df)
-    print_values(graf)
+    #graf = plot_graph(df)
+    #print_values(graf)
+    substances_deaths(df)
     pass
